6905289fd99ed4479d84abfe510c913cf156562c
Dominik Senko <dominik.senko@semasol.de>
2024-02-05 09:24:12 +0100

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.semasol.geotest',
   appName: 'geo-test',
   webDir: 'www',
   server: {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public coordinates: {
+    latitude: number,
+    longitude: number
+  } = {
+      latitude: 0,
+      longitude: 0
+    };
+  public spin: boolean = false;
 
-  constructor() {}
+  constructor(private toastController: ToastController) {
 
+    this.toastController.create({
+      message: 'Request permissions...',
+      duration: 2000,
+      color: 'success'
+    }).then(toast => {
+      toast.present();
+    });
+
+    let perm = Geolocation.requestPermissions();
+
+    this.printCurrentPosition();
+    setInterval(() => {
+      this.printCurrentPosition();
+    }, 7000);
+  }
+
+  printCurrentPosition = async () => {
+    this.spin = true;
+    try {
+      let coord = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coord);
+
+      this.coordinates.latitude = coord.coords.latitude;
+      this.coordinates.longitude = coord.coords.longitude;
+
+      this.toastController.create({
+        message: 'Position gefunden',
+        duration: 2000,
+        color: 'success'
+      }).then(toast => {
+        toast.present();
+      });
+
+    } catch (e) {
+      this.toastController.create({
+        message: '#E: ' + e,
+        duration: 2000,
+        color: 'danger'
+      }).then(toast => {
+        toast.present();
+      });
+    }
+    this.spin = false;
+  };
 }
